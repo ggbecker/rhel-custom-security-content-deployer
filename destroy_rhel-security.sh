a002f2834eabb35a70d2dd0c568eef82992b5330
Alexander Jacocks <alexander@redhat.com>
2020-07-30 07:59:41 -0400

    refactor script to take parameters; make playbook work with podman

--- destroy_rhel-security.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook --ssh-extra-args "-F ssh_config" -e destroy_lab=true -e workshop=rhel-security main.yml
--- run.sh
+++ run.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
-ansible-playbook --ssh-extra-args "-F ssh_config" main.yml
+
+case $1 in
+  "") 
+    echo "usage: $0 docker|podman [rhel-security]"
+    exit 1
+    ;;
+
+  "docker")
+    echo "starting build with docker documentation"
+    ;;
+
+  "podman")
+    echo "starting build with podman documentation"
+    ;;
+
+  *)
+    echo "usage: $0 docker|podman [rhel-security]"
+    exit 1
+    ;;
+esac 
+
+case $2 in
+  "")
+    ansible-playbook --ssh-extra-args "-F ssh_config" -e workshop="rhel-custom-security-content" "$1=true" main.yml
+    ;;
+
+  "rhel-security")
+    ansible-playbook --ssh-extra-args "-F ssh_config" -e workshop="rhel-security" -e "$1=true" main.yml
+    ;;
+
+  *)
+    echo "usage: $0 docker|podman [rhel-security]"
+    exit 1
+    ;;
+esac 
